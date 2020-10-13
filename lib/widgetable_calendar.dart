325a79910cbf9ea0041fe5d5a734a1e3921dfd54
gohdong <aldehf420@naver.com>
2020-10-13 16:02:22 +0900

    develop : #1

--- lib/widgetable_calendar.dart
+++ lib/widgetable_calendar.dart
@@ -1,7 +1,53 @@
 library widgetable_calendar;
 
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
 /// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+class WidgetableCalendar extends StatefulWidget{
+  final Color textColor;
+  final Color backgroundColor;
+  final Color lineColor;
+  final List holidays;
+
+
+
+  WidgetableCalendar(
+      {this.textColor = Colors.black,
+      this.backgroundColor = Colors.white,
+      this.lineColor = Colors.black,
+      this.holidays})
+  :assert(holidays!=null);
+
+  @override
+  _WidgetableCalendarState createState() => _WidgetableCalendarState();
+}
+
+class _WidgetableCalendarState extends State<WidgetableCalendar> {
+  int addOne(int value) {
+    // print(widget.textColor);
+    // print(widget.backgroundColor);
+    // print(widget.lineColor);
+    // print(widget.holidays);
+    return value + 1;
+  }
+
+  int _counter = 0;
+
+
+  void _incrementCounter() {
+    setState(() {
+      // 라이브러리 매소드 사용
+      _counter = _counter+1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 100,
+      color: widget.backgroundColor,
+    );
+  }
 }
--- test/widgetable_calendar_test.dart
+++ test/widgetable_calendar_test.dart
@@ -4,7 +4,7 @@ import 'package:widgetable_calendar/widgetable_calendar.dart';
 
 void main() {
   test('adds one to input values', () {
-    final calculator = Calculator();
+    final calculator = WidgetableCalendar();
     expect(calculator.addOne(2), 3);
     expect(calculator.addOne(-7), -6);
     expect(calculator.addOne(0), 1);
