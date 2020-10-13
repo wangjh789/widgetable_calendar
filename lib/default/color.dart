237f542f781349ac624542e8438e1f914221b6d0
jonghwi Wang <wangjh789@daum.net>
2020-10-13 16:10:13 +0900

    merge upstream/master

---
+++ lib/default/color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class DefaultColor{
+
+  DefaultColor._();
+
+
+  static const Color monthColor = Colors.black;
+  static const Color dayColor = Colors.black;
+  static const Color dateColor = Colors.black;
+  static const Color selectionColor = Colors.amber;
+
+}
---
+++ lib/default/fontSize.dart
@@ -0,0 +1,10 @@
+class DefaultFontSize {
+
+  DefaultFontSize._();
+
+  static const double dayFontSize = 11;
+  static const double monthFontSize = 11;
+  static const double dateFontSize = 11;
+
+
+}
