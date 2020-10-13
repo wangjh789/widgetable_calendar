fd82f978ac8df658fadd3cf6634477c980559425
ToasT1ng <jrainboww9@gmail.com>
2020-10-13 15:42:16 +0900

    simple calendar complete

--- lib/src/calendar.dart
+++ lib/src/calendar.dart
@@ -1,9 +1,7 @@
 part of widgetable_calendar;
 
-
 class WidgetableCalendar extends StatefulWidget {
 //  final DateTime selectDate;
-
 
   WidgetableCalendar({
     Key key,
@@ -13,41 +11,52 @@ class WidgetableCalendar extends StatefulWidget {
   _WidgetableCalendarState createState() => _WidgetableCalendarState();
 }
 
-
-class _WidgetableCalendarState extends State<WidgetableCalendar> with SingleTickerProviderStateMixin {
+class _WidgetableCalendarState extends State<WidgetableCalendar>
+    with SingleTickerProviderStateMixin {
   DateTime selectDate = DateTime.now();
   DateTime firstDay;
   DateTime lastDay;
   List weekList = [];
-  List monthList = ["Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sep","Oct","Nov","Dec"];
   bool todayOrNot;
 
   @override
   void initState() {
     super.initState();
-    firstDay = DateTime(selectDate.year,selectDate.month,1);
-    lastDay = DateTime(selectDate.year,selectDate.month+1,1).subtract(new Duration(days: 1));
-    weekList = _makeWeekList(firstDay,lastDay);
+    firstDay = DateTime(selectDate.year, selectDate.month, 1);
+    lastDay = DateTime(selectDate.year, selectDate.month + 1, 1)
+        .subtract(new Duration(days: 1));
+    weekList = _makeWeekList(firstDay, lastDay);
   }
 
-  List _makeWeekList(DateTime firstDay,DateTime lastDay){
+  List _makeWeekList(DateTime firstDay, DateTime lastDay) {
     List<int> dateList = List<int>();
 
     int firstDayWeekday = firstDay.weekday == 7 ? 0 : firstDay.weekday;
     int lastDayWeekday = lastDay.weekday == 7 ? 0 : lastDay.weekday;
 
     // make long date list ( ex. [0,0,0,0,1,2,3,...,31]
-    for (int i=0; firstDayWeekday != i; i++) dateList.add(0);
-    for (int i=0; lastDay.day != i; i++) dateList.add(i+1);
-    for (int i=lastDayWeekday;  i!= 6 ; i++) dateList.add(0);
+    for (int i = 0; firstDayWeekday != i; i++) dateList.add(0);
+    for (int i = 0; lastDay.day != i; i++) dateList.add(i + 1);
+    for (int i = lastDayWeekday; i != 6; i++) dateList.add(0);
 
     // split with 7 ( make it to week! )  ( ex. [ [0,0,0,0,1,2,3], [4,5,...], ... ] )
     List weekList = [];
     for (var i = 0; i < dateList.length; i += 7) {
-      weekList.add(dateList.sublist(i, i+7 > dateList.length ? dateList.length : i + 7));
+      weekList.add(dateList.sublist(
+          i, i + 7 > dateList.length ? dateList.length : i + 7));
     }
 
     return weekList;
+  }
+
+  bool _isToday(int date) {
+    DateTime today = DateTime.now();
+    if (selectDate.year == today.year &&
+        selectDate.month == today.month &&
+        date == today.day) {
+      return true;
+    } else
+      return false;
   }
 
   @override
@@ -64,38 +73,56 @@ class _WidgetableCalendarState extends State<WidgetableCalendar> with SingleTick
     );
   }
 
-
   // This is Header (  <   2020 october   >  )
   Widget _buildHeader() {
+    List monthList = [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "Jun",
+      "Jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec"
+    ];
+
     final children = [
       IconButton(
         icon: Icon(Icons.arrow_back_ios),
         onPressed: () {
           setState(() {
-            selectDate = DateTime(selectDate.year,selectDate.month-1,1);
-            firstDay = DateTime(selectDate.year,selectDate.month,1);
-            lastDay = DateTime(selectDate.year,selectDate.month+1,1).subtract(new Duration(days: 1));
-            weekList = _makeWeekList(firstDay,lastDay);
+            selectDate = DateTime(selectDate.year, selectDate.month - 1, 1);
+            firstDay = DateTime(selectDate.year, selectDate.month, 1);
+            lastDay = DateTime(selectDate.year, selectDate.month + 1, 1)
+                .subtract(new Duration(days: 1));
+            weekList = _makeWeekList(firstDay, lastDay);
           });
         },
       ),
       Expanded(
         child: Container(
-          child: Center(child: Text(selectDate.year.toString() + " " + monthList[selectDate.month-1])),
+          child: Center(
+              child: Text(selectDate.year.toString() +
+                  " " +
+                  monthList[selectDate.month - 1])),
         ),
       ),
       IconButton(
         icon: Icon(Icons.arrow_forward_ios),
         onPressed: () {
           setState(() {
-            selectDate = DateTime(selectDate.year,selectDate.month+1,1);
-            firstDay = DateTime(selectDate.year,selectDate.month,1);
-            lastDay = DateTime(selectDate.year,selectDate.month+1,1).subtract(new Duration(days: 1));
-            weekList = _makeWeekList(firstDay,lastDay);
+            selectDate = DateTime(selectDate.year, selectDate.month + 1, 1);
+            firstDay = DateTime(selectDate.year, selectDate.month, 1);
+            lastDay = DateTime(selectDate.year, selectDate.month + 1, 1)
+                .subtract(new Duration(days: 1));
+            weekList = _makeWeekList(firstDay, lastDay);
           });
         },
       ),
-
     ];
 
     return Container(
@@ -106,14 +133,11 @@ class _WidgetableCalendarState extends State<WidgetableCalendar> with SingleTick
     );
   }
 
-
   Widget _buildCalendarContent() {
-    final children = <TableRow>[
-      _buildDaysOfWeek()
-    ];
+    final children = <TableRow>[_buildDaysOfWeek()];
 
-    for (int i=0 ; i < weekList.length ; i++){
-      children.add(_buildWeek(weekList[i]));
+    for (int i = 0; i < weekList.length; i++) {
+      children.add(_buildEachWeek(weekList[i]));
     }
 
     return Table(
@@ -122,32 +146,94 @@ class _WidgetableCalendarState extends State<WidgetableCalendar> with SingleTick
   }
 
   // SUN MON TUE WED THU FRI SAT
-  TableRow _buildDaysOfWeek(){
-    return TableRow(
-        children: <Widget>[
-          TableCell(child: Container(width: double.infinity, height: 50, color: Colors.red, child: Center(child: Text("SUN")),)),
-          TableCell(child: Container(width: double.infinity, height: 50, child: Center(child: Text("MON")),)),
-          TableCell(child: Container(width: double.infinity, height: 50, child: Center(child: Text("TUE")),)),
-          TableCell(child: Container(width: double.infinity, height: 50, child: Center(child: Text("WED")),)),
-          TableCell(child: Container(width: double.infinity, height: 50, child: Center(child: Text("THU")),)),
-          TableCell(child: Container(width: double.infinity, height: 50, child: Center(child: Text("FRI")),)),
-          TableCell(child: Container(width: double.infinity, height: 50, color: Colors.blue, child: Center(child: Text("SAT")),)),
-    ]);
-  }
-
-  TableRow _buildWeek(List weekList){
+  TableRow _buildDaysOfWeek() {
+    List dayList = [
+      "SUN",
+      "MON",
+      "TUE",
+      "WED",
+      "THU",
+      "FRI",
+      "SAT",
+    ];
     final children = <TableCell>[];
 
-    for (int i=0 ; i < weekList.length ; i++){
-      String date = weekList[i] !=0 ? weekList[i].toString() : "";
-      children.add(TableCell(child: Container(width: double.infinity, height: 50, child: Center(child: Text(date)),)),);
+    for (int i = 0; i < dayList.length; i++) {
+      children.add(TableCell(
+          child: Container(
+        width: double.infinity,
+        height: 50,
+        child: Center(
+            child: i == 0
+                ? Text(
+                    dayList[i],
+                    style: TextStyle(color: Colors.red),
+                  )
+                : i == 6
+                    ? Text(
+                        dayList[i],
+                        style: TextStyle(color: Colors.blue),
+                      )
+                    : Text(dayList[i])),
+      )));
     }
 
-    return TableRow(
-        children: children
-    );
+    return TableRow(children: children);
   }
 
+  TableRow _buildEachWeek(List weekList) {
+    final children = <TableCell>[];
+
+    for (int i = 0; i < weekList.length; i++) {
+      String date = weekList[i] != 0 ? weekList[i].toString() : "";
+      children.add(
+        _isToday(weekList[i])
+            ? TableCell(
+                child: Container(
+                width: double.infinity,
+                height: 50,
+                color: Colors.grey[300],
+                child: Center(
+                  child:
+                    i == 0
+                        ? Text(
+                      date,
+                      style: TextStyle(color: Colors.red),
+                    )
+                        : i == 6
+                        ? Text(
+                      date,
+                      style: TextStyle(color: Colors.blue),
+                    )
+                        : Text(date)),
+
+//                    child: Text(date)),
+              ))
+            : TableCell(
+                child: Container(
+                width: double.infinity,
+                height: 50,
+                child: Center(
+//                    child: Text(date)
+                    child:
+                    i == 0
+                        ? Text(
+                      date,
+                      style: TextStyle(color: Colors.red),
+                    )
+                        : i == 6
+                        ? Text(
+                      date,
+                      style: TextStyle(color: Colors.blue),
+                    )
+                        : Text(date)),
+                ),
+              ),
+      );
+    }
+
+    return TableRow(children: children);
+  }
 
   Widget _buildTodayButton() {
     final children = [
@@ -157,14 +243,14 @@ class _WidgetableCalendarState extends State<WidgetableCalendar> with SingleTick
           onPressed: () {
             setState(() {
               selectDate = DateTime.now();
-              firstDay = DateTime(selectDate.year,selectDate.month,1);
-              lastDay = DateTime(selectDate.year,selectDate.month+1,1).subtract(new Duration(days: 1));
-              weekList = _makeWeekList(firstDay,lastDay);
+              firstDay = DateTime(selectDate.year, selectDate.month, 1);
+              lastDay = DateTime(selectDate.year, selectDate.month + 1, 1)
+                  .subtract(new Duration(days: 1));
+              weekList = _makeWeekList(firstDay, lastDay);
             });
           },
         ),
       ),
-
     ];
 
     return Container(
@@ -174,6 +260,4 @@ class _WidgetableCalendarState extends State<WidgetableCalendar> with SingleTick
       ),
     );
   }
-
-
 }
