689af4780274768538021851de5d1870e0caf557
jonghwi Wang <wangjh789@daum.net>
2020-10-13 16:00:46 +0900

    add defulat setting

---
+++ example/lib/default/color.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class DefaultColor{
+
+  DefaultColor._();
+
+  static const Color monthColor = Colors.black;
+  static const Color dayColor = Colors.black;
+  static const Color dateColor = Colors.black;
+  static const Color selectionColor = Colors.amber;
+
+}
---
+++ example/lib/default/fontSize.dart
@@ -0,0 +1,9 @@
+class DefaultFontSize {
+
+  DefaultFontSize._();
+
+  static const double dayFontSize = 11;
+  static const double monthFontSize = 11;
+  static const double dateFontSize = 11;
+
+}
